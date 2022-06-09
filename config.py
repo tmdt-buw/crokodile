54603c0192dd4433c55c103436d90e82abde4236
Christian Bitter <bitter@uni-wuppertal.de>
2022-06-09 22:53:14 +0200

    Add default config

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+data_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+
+wandb_config = {
+    "project": "PITL",
+    "entity": "robot2robot",
+
+    "mode": "online"
+}
